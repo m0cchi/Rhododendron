8c6231764a491635282d10096a8b4a08b79b846c
mocchi <boom.boom.planet@gmail.com>
2015-10-08 21:12:29 +0900

    sorter

---
+++ src/main/java/io/mocchi/PathSorter.java
@@ -0,0 +1,25 @@
+package io.mocchi;
+
+import io.mocchi.adaptor.OptimizedImage;
+
+import java.util.Comparator;
+
+public class PathSorter implements Comparator<OptimizedImage> {
+
+	public int compare(String o1, String o2) {
+		char[] s1 = o1.toCharArray();
+		char[] s2 = o2.toCharArray();
+		for (int i = 0; i < s1.length && i < s2.length; i++) {
+			if (s1[i] == s2[i])
+				continue;
+			return s1[i] > s2[i] ? 1 : -1;
+		}
+		return s1.length == s2.length ? 0 : s1.length > s2.length ? 1 : -1;
+	}
+
+	@Override
+	public int compare(OptimizedImage o1, OptimizedImage o2) {
+		return compare(o1.getPath(), o2.getPath());
+	}
+
+}
