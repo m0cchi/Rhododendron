3edbe57097856039d043ee63722ed6a6cbd8e534
mocchi <boom.boom.planet@gmail.com>
2015-10-18 18:15:03 +0900

    add cache size

--- src/main/java/io/mocchi/Settings.java
+++ src/main/java/io/mocchi/Settings.java
@@ -3,4 +3,5 @@ package io.mocchi;
 public class Settings {
 	final public static String APP_NAME = "Rhododendron";
 	final public static String[] FORMATS = { "jpg", "png" };
+	public static int cacheSize = 3;
 }
